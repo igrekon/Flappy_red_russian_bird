75f4aa719fd346350f88dd3049fd97d260891796
igrekon <igorechekneznaika@gmail.com>
2026-03-20 18:35:00 +0200

    After first time

---
+++ core/src/ru/innovationcampus/vsu26/igrekon/happy_flappy_bird/MyGdxGame.java
@@ -0,0 +1,37 @@
+package ru.innovationcampus.vsu26.igrekon.happy_flappy_bird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture birdTexture;
+	int birdX = 0;
+	int birdY = 0;
+	int birdSpeed = 2;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		birdTexture = new Texture("bird0.png");
+	}
+
+	@Override
+	public void render () {
+		birdX+=birdSpeed;
+		birdY+=birdSpeed;
+
+		ScreenUtils.clear(1, 1, 1, 1);
+		batch.begin();
+		batch.draw(birdTexture, birdX, birdY);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		birdTexture.dispose();
+	}
+}
